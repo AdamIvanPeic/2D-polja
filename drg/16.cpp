ad34b6e5b62fc5e7da794d7cec32ea7d149f4928
AdamIvanPeic <122674533+AdamIvanPeic@users.noreply.github.com>
2023-02-25 17:57:22 +0100

    Update 16.cpp

--- drg/16.cpp
+++ drg/16.cpp
@@ -24,14 +24,12 @@ int main(void)
         int c1 = 0, c2 = 0, c3 = 0;
         string s = w.at(i);
         for(int j = 0; j < s.size(); j++)
-        {
             if(s.at(j)=='A' || s.at(j)=='a' || s.at(j)=='S' || s.at(j)=='s' || s.at(j)=='D' || s.at(j)=='d' || s.at(j)=='F' || s.at(j)=='f' || s.at(j)=='G' || s.at(j)=='g' || s.at(j)=='H' || s.at(j)=='h' || s.at(j)=='J' || s.at(j)=='j' || s.at(j)=='K' || s.at(j)=='k' || s.at(j)=='L' || s.at(j)=='l')
                 c2++;
             else if(s.at(j)=='Z' || s.at(j)=='z' || s.at(j)=='X' || s.at(j)=='x' || s.at(j)=='C' || s.at(j)=='c' || s.at(j)=='V' || s.at(j)=='v' || s.at(j)=='B' || s.at(j)=='b' || s.at(j)=='N' || s.at(j)=='n' || s.at(j)=='M' || s.at(j)=='m')
                 c3++;
             else
                 c1++;
-        }
         if(c1 != w.at(i).size() && c2 != w.at(i).size() && c3 != w.at(i).size())
         {
             w.erase(w.begin()+i);
